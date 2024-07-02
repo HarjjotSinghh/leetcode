f23a841107e33b40f90b8a5b63e45f7aba2c5960
Harjot Singh <HarjjotSinghh@gmail.com>
2024-07-02 05:39:42 +0530

    2nd July 2024

---
+++ 2024/July/02_07_2024.py
@@ -0,0 +1,24 @@
+from typing import List
+from collections import Counter
+
+# My Solution
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return (Counter(nums1) & Counter(nums2)).elements()
+
+# Best / Most Optimal Solution
+class Solution2:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        intersection = []
+        nums1.sort()
+        nums2.sort()
+        i = 0
+        for num in nums1:
+            if i >= len(nums2):
+                break
+            while i < len(nums2) - 1 and nums2[i] < num:
+                i += 1
+            if nums2[i] == num:
+                i += 1
+                intersection.append(num)
+        return intersection
