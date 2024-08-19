eb2e0d5a9a7318599b5b50db50ff1dc74d0ccf7d
Harjot Singh <HarjjotSinghh@gmail.com>
2024-08-19 05:35:37 +0530

    19th August 2024

--- 2024/August/19_08_2024.py
+++ 2024/August/19_08_2024.py
@@ -1,1 +1,28 @@
-# This day has not yet come. This file is currently empty.
+
+# My Solution
+class Solution:
+    def minSteps(self, n: int) -> int:
+        ans = 0
+        d = 2
+        while n > 1:
+            while n % d == 0:
+                ans += d
+                n //= d
+            d += 1
+        return ans
+
+# Best / Most Optimal Solution
+class Solution2:
+    def minSteps(self, n: int) -> int:
+        if n==1:
+            return 0
+        currLen=1
+        currCopy=1
+        currOperations=1
+        while currLen<n:
+            if n%currLen == 0 and currLen!=currCopy:
+                currCopy=currLen
+            else:
+                currLen+=currCopy
+            currOperations+=1
+        return currOperations
