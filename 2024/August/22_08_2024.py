3cf980a0ac823053e5936eba01bc556867165c96
Harjot Singh <HarjjotSinghh@gmail.com>
2024-08-22 05:43:19 +0530

    22nd August 2024

--- 2024/August/22_08_2024.py
+++ 2024/August/22_08_2024.py
@@ -1,1 +1,28 @@
-# This day has not yet come. This file is currently empty.
+
+# My Solution
+class Solution:
+    def findComplement(self, num: int) -> int:
+        n=bin(num).split('b')
+        m=n[1]
+        s=''
+        for i in m:
+            if i=='0':
+                s+='1'
+            else:
+                s+='0'
+        return int(s,2)
+    
+# Best / Most Optimal Solution
+class Solution:
+    def findComplement(self, num: int) -> int:
+        s=bin(num)[2:]
+        v=str(s)
+        l=[]
+        for i in v:
+            if i=="0":
+                l.append("1")
+            else:
+                l.append("0")
+        v=''.join(l)
+        b=int(v,2)
+        return b
