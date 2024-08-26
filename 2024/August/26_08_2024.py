1fafb9f82443bcb71417c105b86420ee728c434f
Harjot Singh <HarjjotSinghh@gmail.com>
2024-08-26 05:55:25 +0530

    26th August 2024

--- 2024/August/26_08_2024.py
+++ 2024/August/26_08_2024.py
@@ -1,1 +1,33 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+# My Solution
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        arr = []
+        def recursion(node):
+            if not node:
+                return
+            for child in node.children:
+                recursion(child)
+            arr.append(node.val)
+        recursion(root)
+        return arr
+
+# Best / Most Optimal Solution
+class Solution2:
+    def postorder(self, root: 'Node') -> List[int]:
+        ans = []
+        def dfs(node):
+            if not node :
+                return 
+            for child in node.children:
+                dfs(child)
+            ans.append(node.val)
+        dfs(root)
+        return ans
