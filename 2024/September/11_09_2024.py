4472af6f78b3d627c961b082ac7790790f6e626d
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-09-11 20:58:17 +0530

    11th September 2024

--- 2024/September/11_09_2024.py
+++ 2024/September/11_09_2024.py
@@ -1,1 +1,21 @@
-# This day has not yet come. This file is currently empty.
+
+# My Solution
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        xor_result = start ^ goal
+        count = 0
+        while xor_result:
+            xor_result &= xor_result - 1
+            count += 1
+        return count
+
+# Best / Most Optimal Solution
+class Solution2:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        c = 0
+        while start or goal:
+            if start%2 != goal%2:
+                c+=1
+            start = start//2
+            goal = goal//2
+        return c
