0910f8eb0b183697143b91a72bd59b374f7829f9
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-09-20 05:40:24 +0530

    20th September 2024

--- 2024/September/20_09_2024.py
+++ 2024/September/20_09_2024.py
@@ -1,1 +1,33 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        reversed_string = s[::-1]
+        combined_string = s + "#" + reversed_string
+        prefix_table = self._build_prefix_table(combined_string)
+        palindrome_length = prefix_table[-1]
+        suffix = reversed_string[: len(s) - palindrome_length]
+        return suffix + s
+    def _build_prefix_table(self, s: str) -> list:
+        prefix_table = [0] * len(s)
+        length = 0
+        for i in range(1, len(s)):
+            while length > 0 and s[i] != s[length]:
+                length = prefix_table[length - 1]
+            if s[i] == s[length]:
+                length += 1
+            prefix_table[i] = length
+        return prefix_table
+
+# Best / Most Optimal Solution
+class Solution2:
+    def shortestPalindrome(self, s: str) -> str:
+        i = 0
+        n = len(s)
+        for j in range(n):
+            if s[i] == s[n-j-1]:
+                i += 1
+        if i==n:
+            return s
+        p = s[i:n][::-1]
+        return p + self.shortestPalindrome(s[:i]) + s[i:]
+
