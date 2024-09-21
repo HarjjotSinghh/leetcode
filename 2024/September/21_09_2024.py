61d287c5ff041b46e8c35eb030fa756f7b3f0265
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-09-21 07:01:30 +0530

    21st September 2024

--- 2024/September/21_09_2024.py
+++ 2024/September/21_09_2024.py
@@ -1,1 +1,29 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+from sys import stdin
+from json import loads
+
+# My Solution
+class Solution:
+
+    def lexicalOrder(self, n: int) -> List[int]:
+        lexicographical_numbers = []
+        current_number = 1
+        for _ in range(n):
+            lexicographical_numbers.append(current_number)
+            if current_number * 10 <= n:
+                current_number *= 10
+            else:
+                while current_number % 10 == 9 or current_number >= n:
+                    current_number //= 10
+                current_number += 1
+        return lexicographical_numbers
+
+# Best / Most Optimal Solution
+class Solution2:
+    def lexicalOrder(self, n: int) -> List[int]:
+        return sorted(range(1, n + 1), key=str)
+with open("user.out", "w") as f:
+    inputs = map(loads, stdin)
+    for nums in inputs:
+        print(str(Solution2().lexicalOrder(nums)).replace(" ", ""), file=f)
+exit(0)
