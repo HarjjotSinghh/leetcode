ad5d7416e17014c8a002b80246bd945410819a15
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-09-22 10:44:00 +0530

    22nd September 2024

--- 2024/September/22_09_2024.py
+++ 2024/September/22_09_2024.py
@@ -1,1 +1,45 @@
-# This day has not yet come. This file is currently empty.
+
+# My Solution
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        curr = 1
+        k -= 1
+        while k > 0:
+            step = self._count_steps(n, curr, curr + 1)
+            if step <= k:
+                curr += 1
+                k -= step
+            else:
+                curr *= 10
+                k -= 1
+        return curr
+    def _count_steps(self, n: int, prefix1: int, prefix2: int) -> int:
+        steps = 0
+        while prefix1 <= n:
+            steps += min(n + 1, prefix2) - prefix1
+            prefix1 *= 10
+            prefix2 *= 10
+        return steps
+
+# Best / Most Optimal Solution
+class Solution2:
+    def findKthNumber(self, n: int, k: int) -> int:
+        def calc_steps(curr, n):
+            steps = 0
+            nxt = curr + 1
+            while curr <= n:
+                steps += min(n + 1, nxt) - curr
+                curr *= 10
+                nxt *= 10
+            return steps
+        curr = 1
+        k -= 1
+        while k > 0:
+            steps = calc_steps(curr, n)
+            if steps <= k:
+                k -= steps
+                curr += 1
+            else:
+                curr *= 10
+                k -= 1
+        return curr
