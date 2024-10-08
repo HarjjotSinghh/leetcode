44f1275a6b84c62a381890fed7df807f929c9712
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-10-08 05:49:54 +0530

    8th October 2024

--- 2024/October/08_10_2024.py
+++ 2024/October/08_10_2024.py
@@ -1,1 +1,23 @@
-# This day has not yet come. This file is currently empty.
+
+# My Solution
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        stack_size = 0
+        for ch in s:
+            if ch == "[":
+                stack_size += 1
+            else:
+                if stack_size > 0:
+                    stack_size -= 1
+        return (stack_size + 1) // 2
+
+# Best / Most Optimal Solution
+class Solution:
+  def minSwaps(self, s: str) -> int:
+    unmatched = 0
+    for c in s:
+      if c == '[':
+        unmatched += 1
+      elif unmatched > 0:
+        unmatched -= 1
+    return (unmatched + 1) // 2
