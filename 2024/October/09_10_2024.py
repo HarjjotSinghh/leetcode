ce4d70645d0d5841411760b6a473d845ec4af92e
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-10-09 08:15:13 +0530

    9th October 2024

--- 2024/October/09_10_2024.py
+++ 2024/October/09_10_2024.py
@@ -1,1 +1,30 @@
-# This day has not yet come. This file is currently empty.
+
+# My Soluiton
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        open_brackets = 0
+        min_adds_required = 0
+        for c in s:
+            if c == "(":
+                open_brackets += 1
+            else:
+                if open_brackets > 0:
+                    open_brackets -= 1
+                else:
+                    min_adds_required += 1
+        return min_adds_required + open_brackets
+
+# Best / Most Optimal Solution
+class Solution2:
+    def minAddToMakeValid(self, s: str) -> int:
+        need = 0
+        res = 0
+        for p in s:
+            if p == '(':
+                need += 1
+            if p == ')':
+                need -= 1
+                if need == -1:
+                    res += 1
+                    need = 0
+        return res + need
