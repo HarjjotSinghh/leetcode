102cf20b52c95aa4c716beedabd3524da4fc4b16
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-10-15 08:32:19 +0530

    15th October 2024

--- 2024/October/15_10_2024.py
+++ 2024/October/15_10_2024.py
@@ -1,1 +1,23 @@
-# This day has not yet come. This file is currently empty.
+
+# My Solution
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        total_swaps = 0
+        black_ball_count = 0
+        for char in s:
+            if char == "0":
+                total_swaps += black_ball_count
+            else:
+                black_ball_count += 1
+        return total_swaps
+
+# Best / Most Optimal Solution
+class Solution2:
+    def minimumSteps(self, s: str) -> int:
+        res, swaps = 0, 0
+        for ch in s:
+            if ch == '1':
+                swaps += 1
+            else:
+                res += swaps
+        return res     
