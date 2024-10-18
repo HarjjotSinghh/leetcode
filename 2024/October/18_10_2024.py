ddad6773d862940c8eb64307f63afd5511902911
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-10-18 09:47:26 +0530

    18th October 2024

--- 2024/October/18_10_2024.py
+++ 2024/October/18_10_2024.py
@@ -1,1 +1,30 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+from collections import Counter
+
+
+# My Solution
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        max_or_value = 0
+        dp = [0] * (1 << 17)
+        dp[0] = 1
+        for num in nums:
+            for i in range(max_or_value, -1, -1):
+                dp[i | num] += dp[i]
+            max_or_value |= num
+        return dp[max_or_value]
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        max_or = 0
+        prev = Counter()
+        prev[0] = 1
+        for elem in nums:
+            max_or |= elem
+            current = Counter()
+            for prev_or, cnt in prev.items():
+                current[prev_or | elem] += cnt
+            prev.update(current)
+        return prev[max_or]
