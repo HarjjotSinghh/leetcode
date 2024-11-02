9de7c3b27bee0c5ff216308e594d79abf0936b0c
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-02 10:23:58 +0530

    2nd November 2024

--- 2024/June/28_06_2024.py
+++ 2024/June/28_06_2024.py
@@ -1,23 +1,26 @@
 from typing import List
+from operator import mul
+
 
 # My Solution
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
         arr = [0] * n
-        for a,b in roads:
-            arr[a]+= 1
-            arr[b]+= 1
-        return sum(map(mul, sorted(arr), range(1,1+n)))
-    
+        for a, b in roads:
+            arr[a] += 1
+            arr[b] += 1
+        return sum(map(mul, sorted(arr), range(1, 1 + n)))
+
+
 # Best / Most Optimal Solution
 class Solution2:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
         Arr = [0] * n
-        for A,B in roads:
+        for A, B in roads:
             Arr[A] += 1
             Arr[B] += 1
         Arr.sort()
         summ = 0
         for i in range(len(Arr)):
-            summ += Arr[i] * (i+1)
+            summ += Arr[i] * (i + 1)
         return summ
--- 2024/November/02_11_2024.py
+++ 2024/November/02_11_2024.py
@@ -1,1 +1,31 @@
-# This day has not yet come. This file is currently empty.
+
+
+# My Solution
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        words = sentence.split(" ")
+        n = len(words)
+        last = words[n - 1][-1]
+        for i in range(n):
+            if words[i][0] != last:
+                return False
+            last = words[i][-1]
+        return True
+    
+# Best / Most Optimal Solution
+class Solution2:
+    def isCircularSentence(self, sentence: str) -> bool:
+        if sentence[0] != sentence[-1]:
+            return False
+        s = sentence.split()
+        print(s,s[0],s[-1])
+        if len(s) == 1:
+            s=''.join(s)
+            if s[0] == s[-1]:
+                return True
+            else:
+                return False
+        for i in range(len(s)-1):
+            if s[i][-1] != s[i+1][0]:
+                return False
+        return True
