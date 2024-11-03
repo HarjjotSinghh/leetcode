6aad1d26194254d2fcdc4ea6d1db93b393917bd0
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-03 06:52:09 +0530

    3rd November 2024

--- 2024/November/03_11_2024.py
+++ 2024/November/03_11_2024.py
@@ -1,1 +1,16 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        doubled_string = s + s
+        return doubled_string.find(goal) != -1
+
+
+# Best / Most Optimal Solution
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for i in range(len(s)):
+            if s[i:] + s[:i] == goal:
+                return True
+        return False
