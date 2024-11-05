aac9e5432023e1605effd1afab9e41f34a711881
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-05 13:08:35 +0530

    5th November 2024

--- 2024/November/05_11_2024.py
+++ 2024/November/05_11_2024.py
@@ -1,1 +1,14 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def minChanges(self, s: str) -> int:
+        changes = 0
+        for i in range(0, len(s), 2):
+            if s[i] != s[i + 1]:
+                changes += 1
+        return changes
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def minChanges(self, s: str) -> int:
+        return sum(s[i] != s[i + 1] for i in range(0, len(s), 2))
