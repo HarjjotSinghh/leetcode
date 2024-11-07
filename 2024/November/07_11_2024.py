9d61f8dc62965ae225efe14c1d92884b4d27c5a5
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-07 10:54:36 +0530

    7th November 2024

--- 2024/November/07_11_2024.py
+++ 2024/November/07_11_2024.py
@@ -1,1 +1,24 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        return max(sum(c >> i & 1 for c in candidates) for i in range(24))
+
+
+# Best / Most Optimal Solution
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        MAX_N_BITS = 24
+        max_set = 1  # Any one element is > 0
+        mask = 1
+        for shift in range(MAX_N_BITS):
+            count = 0
+            for num in candidates:
+                if num & mask:
+                    count += 1
+            if max_set < count:
+                max_set = count
+            mask <<= 1
+        return max_set
