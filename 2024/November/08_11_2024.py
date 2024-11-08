56650a8ed2426e4635930137978add221be773fa
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-08 08:15:17 +0530

    8th November 2024

--- 2024/November/08_11_2024.py
+++ 2024/November/08_11_2024.py
@@ -1,1 +1,25 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        prefix_xor = [0] * len(nums)
+        prefix_xor[0] = nums[0]
+        for i in range(1, len(nums)):
+            prefix_xor[i] = prefix_xor[i - 1] ^ nums[i]
+        ans = [0] * len(nums)
+        mask = (1 << maximumBit) - 1
+        for i in range(len(nums)):
+            current_xor = prefix_xor[len(prefix_xor) - 1 - i]
+            ans[i] = current_xor ^ mask
+        return ans
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        ans = [(1 << maximumBit) - 1]
+        for n in nums:
+            ans.append(ans[-1] ^ n)
+        return ans[len(ans) - 1 : 0 : -1]
