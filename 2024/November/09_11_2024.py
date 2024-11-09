ae0b3f26d5da558b43846f143da83602bc7be6ec
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-09 11:15:08 +0530

    9th November 2024

--- 2024/November/09_11_2024.py
+++ 2024/November/09_11_2024.py
@@ -1,1 +1,56 @@
-# This day has not yet come. This file is currently empty.
+from math import floor
+
+
+# My Solution
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        result = x
+        n -= 1
+        mask = 1
+        while n > 0:
+            if (mask & x) == 0:
+                result |= (n & 1) * mask
+                n >>= 1
+            mask <<= 1
+        return result
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def minEnd(self, n: int, x: int) -> int:
+        if n == 1:
+            return x
+        one_count = x.bit_count()
+        k = len(bin(x)) - 2
+        max_ = 2**k - 1
+        if one_count == k:
+            size = 1
+        else:
+            size = 2 ** (k - one_count)
+        if floor((n - 1) / size) > 0:
+            a = floor((n - 1) / size) << k
+        else:
+            a = 0
+        result = a
+        left = (n - 1) % size
+        x = bin(x)[2:]
+        t = ""
+        left = bin(left)[2:]
+        limit = len(left)
+        i = limit - 1
+        print(left, result)
+        print(x)
+        for k in x[::-1]:
+            print("k", k, t)
+            if k == "1":
+                t = "1" + t
+            elif i >= 0:
+                if left[i] == "1":
+                    t = "1" + t
+                else:
+                    t = "0" + t
+                i -= 1
+            else:
+                t = "0" + t
+        result += int(t, 2)
+        return result
