cc7111dd81b4c3d35364832330f88592c7853289
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-10 10:17:34 +0530

    10th November 2024

--- 2024/November/10_11_2024.py
+++ 2024/November/10_11_2024.py
@@ -1,1 +1,47 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+from math import inf
+
+
+# My Solution
+class Solution:
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        min_length = float("inf")
+        window_start = window_end = 0
+        bit_counts = [0] * 32
+        while window_end < len(nums):
+            self._update_bit_counts(bit_counts, nums[window_end], 1)
+            while (
+                window_start <= window_end
+                and self._convert_bits_to_num(bit_counts) >= k
+            ):
+                min_length = min(min_length, window_end - window_start + 1)
+                self._update_bit_counts(bit_counts, nums[window_start], -1)
+                window_start += 1
+            window_end += 1
+        return -1 if min_length == float("inf") else min_length
+
+    def _update_bit_counts(self, bit_counts: list, number: int, delta: int) -> None:
+        for pos in range(32):
+            if number & (1 << pos):
+                bit_counts[pos] += delta
+
+    def _convert_bits_to_num(self, bit_counts: list) -> int:
+        result = 0
+        for pos in range(32):
+            if bit_counts[pos]:
+                result |= 1 << pos
+        return result
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        ans = inf
+        d = dict()
+        for i, x in enumerate(nums):
+            d = {or_ | x: left for or_, left in d.items()}
+            d[x] = i
+            for or_, left in d.items():
+                if or_ >= k:
+                    ans = min(ans, i - left + 1)
+        return ans if ans < inf else -1
