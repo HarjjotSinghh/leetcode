5f96171000c2dd96b625a49ea682584f563408bf
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-15 06:00:05 +0530

    15th November 2024

--- 2024/November/15_11_2024.py
+++ 2024/November/15_11_2024.py
@@ -1,1 +1,40 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        right = len(arr) - 1
+        while right > 0 and arr[right] >= arr[right - 1]:
+            right -= 1
+        ans = right
+        left = 0
+        while left < right and (left == 0 or arr[left - 1] <= arr[left]):
+            while right < len(arr) and arr[left] > arr[right]:
+                right += 1
+            ans = min(ans, right - left - 1)
+            left += 1
+        return ans
+
+
+# Best / Most Optimal Solution
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        n = len(arr)
+        left = 0
+        while left + 1 < n and arr[left] <= arr[left + 1]:
+            left += 1
+        if left == n - 1:
+            return 0
+        right = n - 1
+        while right > 0 and arr[right - 1] <= arr[right]:
+            right -= 1
+        min_remove = min(n - left - 1, right)
+        i, j = 0, right
+        while i <= left and j < n:
+            if arr[i] <= arr[j]:
+                min_remove = min(min_remove, j - i - 1)
+                i += 1
+            else:
+                j += 1
+        return min_remove
