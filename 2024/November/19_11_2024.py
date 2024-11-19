54cb4428e8e1c4687c7b2ecc239e6595195fb536
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-19 07:25:57 +0530

    19th November 2024

--- 2024/November/19_11_2024.py
+++ 2024/November/19_11_2024.py
@@ -1,1 +1,50 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        current_sum = 0
+        begin = 0
+        end = 0
+        num_to_index = {}
+        while end < len(nums):
+            curr_num = nums[end]
+            last_occurrence = num_to_index.get(curr_num, -1)
+            while begin <= last_occurrence or end - begin + 1 > k:
+                current_sum -= nums[begin]
+                begin += 1
+            num_to_index[curr_num] = end
+            current_sum += nums[end]
+            if end - begin + 1 == k:
+                ans = max(ans, current_sum)
+            end += 1
+        return ans
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        elements = set()
+        current_sum = 0
+        max_sum = 0
+        begin = 0
+        for end in range(n):
+            if nums[end] not in elements:
+                current_sum += nums[end]
+                elements.add(nums[end])
+                if end - begin + 1 == k:
+                    if current_sum > max_sum:
+                        max_sum = current_sum
+                    current_sum -= nums[begin]
+                    elements.remove(nums[begin])
+                    begin += 1
+            else:
+                while nums[begin] != nums[end]:
+                    current_sum -= nums[begin]
+                    elements.remove(nums[begin])
+                    begin += 1
+                begin += 1
+        return max_sum
