503ea305824149b5412ad256b9331b39fa03a34d
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-22 05:44:16 +0530

    22nd November 2024

--- 2024/November/22_11_2024.py
+++ 2024/November/22_11_2024.py
@@ -1,1 +1,24 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+from collections import Counter
+
+
+# My Solution
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        pattern_frequency = {}
+        for current_row in matrix:
+            row_pattern = "".join(
+                "T" if num == current_row[0] else "F" for num in current_row
+            )
+            pattern_frequency[row_pattern] = pattern_frequency.get(row_pattern, 0) + 1
+        return max(pattern_frequency.values(), default=0)
+
+
+# Best / Most Optimal Solution
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        cnt = Counter()
+        for row in matrix:
+            t = tuple(row) if row[0] == 0 else tuple(x ^ 1 for x in row)
+            cnt[t] += 1
+        return max(cnt.values())
