908f941ca6dea8afc7788fc464a3353f23fee633
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-11-24 10:35:09 +0530

    24th November 2024

--- 2024/November/24_11_2024.py
+++ 2024/November/24_11_2024.py
@@ -1,1 +1,39 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+from math import inf
+
+
+# My Solution
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        total_sum = 0
+        min_abs_val = float("inf")
+        negative_count = 0
+        for row in matrix:
+            for val in row:
+                total_sum += abs(val)
+                if val < 0:
+                    negative_count += 1
+                min_abs_val = min(min_abs_val, abs(val))
+        if negative_count % 2 != 0:
+            total_sum -= 2 * min_abs_val
+        return total_sum
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        total_sum = 0
+        negative_count = 0
+        minel = inf
+        for i in matrix:
+            for j in i:
+                if j < 0:
+                    j = -j
+                    negative_count += 1
+                total_sum += j
+                if j < minel:
+                    minel = j
+        if negative_count % 2 == 0:
+            return total_sum
+        else:
+            return total_sum - 2 * minel
