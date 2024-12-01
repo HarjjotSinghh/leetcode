901b148bb451fa61a08f49dc6621e096560b971b
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-12-01 06:10:02 +0530

    1st December 2024 🆕

--- 2024/December/01_12_2024.py
+++ 2024/December/01_12_2024.py
@@ -1,1 +1,21 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+# My Solution
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        seen = set()
+        for num in arr:
+            if 2 * num in seen or (num % 2 == 0 and num // 2 in seen):
+                return True
+            seen.add(num)
+        return False
+
+# Best / Most Optimal Solution
+class Solution2:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        ls=set()
+        for i in arr:
+            if 2*i in ls or(i%2==0 and i//2 in ls):
+                return True
+            ls.add(i)
+        return False
