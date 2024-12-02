01c0866e13354ce4e3db138caff9dcce22b2f7d9
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-12-02 10:37:32 +0530

    2nd December 2024

--- 2024/December/02_12_2024.py
+++ 2024/December/02_12_2024.py
@@ -1,1 +1,17 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split()
+        for i, word in enumerate(words, 1):
+            if word[: len(searchWord)] == searchWord:
+                return i
+        return -1
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        return next(
+            (i for i, w in enumerate(sentence.split(), 1) if w.startswith(searchWord)),
+            -1,
+        )
