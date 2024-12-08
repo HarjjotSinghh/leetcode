438d26f4627861bfdc69c31acde2a0b1df79eaa4
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-12-08 09:42:32 +0530

    8th December 2024

--- 2024/December/08_12_2024.py
+++ 2024/December/08_12_2024.py
@@ -1,1 +1,35 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+from bisect import bisect_right
+
+
+# My Solution
+class Solution:
+    def maxTwoEvents(self, events):
+        times = []
+        for e in events:
+            times.append([e[0], 1, e[2]])
+            times.append([e[1] + 1, 0, e[2]])
+        ans, max_value = 0, 0
+        times.sort()
+        for time_value in times:
+            if time_value[1]:
+                ans = max(ans, time_value[2] + max_value)
+            else:
+                max_value = max(max_value, time_value[2])
+        return ans
+
+# Best / Most Optimal Solution
+class Solution2:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        max_weights = [0]
+        max_weight_ends = [-1]
+        events.sort(key = lambda x: x[1])
+        max_two = 0
+        for start, end, weight in events:
+            index = bisect_right(max_weight_ends, start - 1) - 1
+            if weight + max_weights[index] > max_two:
+                max_two = weight + max_weights[index]
+            if weight > max_weights[-1]:
+                max_weights.append(weight)
+                max_weight_ends.append(end)
+        return max_two
