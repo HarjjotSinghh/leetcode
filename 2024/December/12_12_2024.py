116afc07d247e1c721b2c36c43dbef62b8eaa628
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-12-12 05:37:51 +0530

    12th December 2024

--- 2024/December/12_12_2024.py
+++ 2024/December/12_12_2024.py
@@ -1,1 +1,28 @@
-# This day has not yet come. This file is currently empty.
+import math
+from typing import List
+import heapq
+
+# My Soluion
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        gifts_heap = [-gift for gift in gifts]
+        heapq.heapify(gifts_heap)
+        for _ in range(k):
+            max_element = -heapq.heappop(gifts_heap)
+            heapq.heappush(gifts_heap, -math.floor(math.sqrt(max_element)))
+        number_of_remaining_gifts = 0
+        while gifts_heap:
+            number_of_remaining_gifts -= heapq.heappop(gifts_heap)
+        return number_of_remaining_gifts
+# Best / Most Optimal Soluion
+class Solution2:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        heap = [-v for v in gifts]
+        heapq.heapify(heap)
+        while k > 0:
+            v = heapq.heappop(heap)
+            v = -v
+            v = math.floor(v ** 0.5)
+            heapq.heappush(heap, -v)
+            k -= 1
+        return -1 * sum(heap)
