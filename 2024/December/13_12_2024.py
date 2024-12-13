534c62a0b663c64b8c4ee2a63f8de2b8eb88c608
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-12-13 17:52:10 +0530

    13th December 2024

--- 2024/December/13_12_2024.py
+++ 2024/December/13_12_2024.py
@@ -1,1 +1,46 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+from collections import deque
+
+# My Solution
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        stk = []
+        res = 0
+        for i in range(len(nums)):
+            if not stk or nums[i] < stk[-1]:
+                stk.append(nums[i])
+            else:
+                while stk:
+                    res += stk.pop()
+                    if stk:
+                        stk.pop()
+        while stk:
+            res += stk.pop()
+            if stk:
+                stk.pop()
+        return res
+
+# Best / Most Optimal Soluion
+class Solution2:
+    def findScore(self, nums: List[int]) -> int:
+        score = 0
+        n = len(nums)
+        q = deque()
+        for i in range(n):
+            if q and nums[i]>=q[-1]:
+                skip=False
+                while q:
+                    add = q.pop()
+                    if not skip:
+                        score += add
+                    skip = not skip
+                continue
+
+            q.append(nums[i])
+        skip=False
+        while q:
+            add = q.pop()
+            if not skip:
+                score += add
+            skip = not skip
+        return score
