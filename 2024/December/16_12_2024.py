9d4afeb3a648669aa9409226d8233e02d7daec12
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-12-16 05:52:54 +0530

    16th December 2024

--- 2024/December/16_12_2024.py
+++ 2024/December/16_12_2024.py
@@ -1,1 +1,21 @@
-# This day has not yet come. This file is currently empty.
+from heapq import heapify, heappop, heappush
+from typing import List
+
+# My Solution
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int):
+        pq = [(val, i) for i, val in enumerate(nums)]
+        heapify(pq)
+        for _ in range(k):
+            _, i = heappop(pq)
+            nums[i] *= multiplier
+            heappush(pq, (nums[i], i))
+        return nums
+
+# Best / Most Optimal Solution
+class Solution2:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        for _ in range(k):
+            idx = nums.index(min(nums))
+            nums[idx] *= multiplier
+        return nums
