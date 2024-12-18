0bbb09694f04bdae9ff20c263a241793b85247a4
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-12-18 14:23:53 +0530

    18th December 2024

--- 2024/December/18_12_2024.py
+++ 2024/December/18_12_2024.py
@@ -1,1 +1,24 @@
-# This day has not yet come. This file is currently empty.
+from collections import deque
+from typing import List
+
+# My Solution
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        result = prices.copy()
+        stack = deque()
+        for i in range(len(prices)):
+            while stack and prices[stack[-1]] >= prices[i]:
+                result[stack.pop()] -= prices[i]
+            stack.append(i)
+        return result
+
+# Best / Most Optimal Solution
+class Solution2:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        ans = prices[:]
+        for i , price in enumerate(prices):
+            while stack and prices[stack[-1]] >= price:
+                ans[stack.pop()]-= price
+            stack.append(i)
+        return ans
