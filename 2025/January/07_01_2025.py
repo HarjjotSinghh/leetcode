f0a2a9b423846980748e3b8a5c008ef7aefde717
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-01-07 05:52:38 +0530

    7th January 2025

--- 2025/January/07_01_2025.py
+++ 2025/January/07_01_2025.py
@@ -1,1 +1,23 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def stringMatching(self, words):
+        matching_words = []
+        for current_word_index in range(len(words)):
+            for other_word_index in range(len(words)):
+                if current_word_index == other_word_index:
+                    continue
+                if words[current_word_index] in words[other_word_index]:
+                    matching_words.append(words[current_word_index])
+                    break
+        return matching_words
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        r = " ".join(words)
+        s = [i for i in words if r.count(i) > 1]
+        return s
