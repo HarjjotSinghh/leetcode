059224efd890c5d6b6da428957b8533b28c2ae83
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-01-08 11:34:40 +0530

    8th January 2025

--- 2025/January/08_01_2025.py
+++ 2025/January/08_01_2025.py
@@ -1,1 +1,29 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        n = len(words)
+        count = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                str1 = words[i]
+                str2 = words[j]
+                if len(str1) > len(str2):
+                    continue
+                if str2.startswith(str1) and str2.endswith(str1):
+                    count += 1
+        return count
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        n = len(words)
+        count = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if words[j].startswith(words[i]) and words[j].endswith(words[i]):
+                    count += 1
+        return count
