328733eb9335a20378ee3ae692880585759c2466
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-01-09 05:32:27 +0530

    9th January 2025

--- 2025/January/09_01_2025.py
+++ 2025/January/09_01_2025.py
@@ -1,1 +1,18 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        return sum(word.startswith(pref) for word in words)
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        pref_length = len(pref)
+        result = 0
+        for word in words:
+            if len(word) >= pref_length and word[0:pref_length] == pref:
+                result += 1
+        return result
