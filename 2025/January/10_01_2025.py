319e3474962b33785ac46f806f00403dc80a9627
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-01-10 15:25:05 +0530

    10th January 2025

--- 2025/January/10_01_2025.py
+++ 2025/January/10_01_2025.py
@@ -1,1 +1,39 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution(object):
+    def wordSubsets(self, A, B):
+        def count(word):
+            ans = [0] * 26
+            for letter in word:
+                ans[ord(letter) - ord("a")] += 1
+            return ans
+
+        bmax = [0] * 26
+        for b in B:
+            for i, c in enumerate(count(b)):
+                bmax[i] = max(bmax[i], c)
+        ans = []
+        for a in A:
+            if all(x >= y for x, y in zip(count(a), bmax)):
+                ans.append(a)
+        return ans
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        ans = set(words1)
+        letters = {}
+        for i in words2:
+            for j in i:
+                count = i.count(j)
+                if j not in letters or count > letters[j]:
+                    letters[j] = count
+        for i in words1:
+            for j in letters:
+                if i.count(j) < letters[j]:
+                    ans.remove(i)
+                    break
+        return list(ans)
