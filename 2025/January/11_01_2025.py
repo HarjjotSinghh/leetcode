147198af78653279a828baea269b5ac297998160
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-01-11 08:32:19 +0530

    11th January 2025

--- 2025/January/11_01_2025.py
+++ 2025/January/11_01_2025.py
@@ -1,1 +1,27 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+        if len(s) == k:
+            return True
+        odd_count = 0
+        for chr in s:
+            odd_count ^= 1 << (ord(chr) - ord("a"))
+        return bin(odd_count).count("1") <= k
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) == k:
+            return True
+        if len(s) < k:
+            return False
+        odd = 0
+        for char in set(s):
+            if s.count(char) % 2:
+                odd += 1
+        if odd > k:
+            return False
+        return True
