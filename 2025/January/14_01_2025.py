fc223637cba2d657d53b21ca7961261b98b99733
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-01-14 09:17:51 +0530

    14th January 2025

--- 2025/January/14_01_2025.py
+++ 2025/January/14_01_2025.py
@@ -1,1 +1,39 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        n = len(A)
+        prefix_common_array = [0 for _ in range(n)]
+        frequency = [0 for _ in range(n + 1)]
+        common_count = 0
+        for current_index in range(n):
+            frequency[A[current_index]] += 1
+            if frequency[A[current_index]] == 2:
+                common_count += 1
+            frequency[B[current_index]] += 1
+            if frequency[B[current_index]] == 2:
+                common_count += 1
+            prefix_common_array[current_index] = common_count
+        return prefix_common_array
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        n = len(A)
+        C = [0] * n
+        seta, setb = set(), set()
+        for i in range(n):
+            C[i] = C[i - 1]
+            if A[i] == B[i]:
+                C[i] += 1
+            else:
+                if A[i] in setb:
+                    C[i] += 1
+                if B[i] in seta:
+                    C[i] += 1
+                seta.add(A[i])
+                setb.add(B[i])
+        return C
