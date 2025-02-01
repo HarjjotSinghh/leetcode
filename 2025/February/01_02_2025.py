7dfba709f9b5a6bf8685d2e9696f44b1fc16e7b7
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-01 07:50:24 +0530

    1st February 2025

--- 2025/February/01_02_2025.py
+++ 2025/February/01_02_2025.py
@@ -1,1 +1,27 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        lastParity = nums[0] % 2
+        for num in nums[1:]:
+            parity = num % 2
+            if parity == lastParity:
+                return False
+            else:
+                lastParity = parity
+        return True
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        lastParity = nums[0] % 2
+        for num in nums[1:]:
+            parity = num % 2
+            if parity == lastParity:
+                return False
+            else:
+                lastParity = parity
+        return True
