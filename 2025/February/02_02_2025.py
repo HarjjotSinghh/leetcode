10628f5806ccb8540245d94d0445f76fbeff3ba5
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-02 06:38:52 +0530

    2nd February 2025

--- 2025/February/02_02_2025.py
+++ 2025/February/02_02_2025.py
@@ -1,1 +1,27 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n <= 1:
+            return True
+        inversion_count = 0
+        for index in range(1, n):
+            if nums[index] < nums[index - 1]:
+                inversion_count += 1
+        if nums[0] < nums[n - 1]:
+            inversion_count += 1
+        return inversion_count <= 1
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def check(self, nums: List[int]) -> bool:
+        sorted_nums = list(sorted(nums))
+        for i in range(len(nums)):
+            if sorted_nums == nums:
+                return True
+            sorted_nums = sorted_nums[1:] + [sorted_nums[0]]
+        return False
