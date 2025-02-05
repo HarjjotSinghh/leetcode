08e633085b6026b2bebd3b79483a865c68d7cfbb
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-05 10:04:53 +0530

    4th February 2025

--- 2025/February/05_02_2025.py
+++ 2025/February/05_02_2025.py
@@ -1,1 +1,25 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        return (
+            not (d := [(a, b) for a, b in zip(s1, s2) if a != b])
+            or len(d) == 2
+            and d[0][0] == d[1][1]
+            and d[0][1] == d[1][0]
+        )
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1 == s2:
+            return True
+        if sorted(s1) != sorted(s2):
+            return False
+        count = 0
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                count += 1
+        if count != 2:
+            return False
+        return True
