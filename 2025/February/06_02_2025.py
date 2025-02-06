caa56568975294169e35e4fcae25250c4413e23a
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-06 05:53:02 +0530

    6th February 2025

--- 2025/February/06_02_2025.py
+++ 2025/February/06_02_2025.py
@@ -1,1 +1,28 @@
-# This day has not yet come. This file is currently empty.
+from collections import Counter
+from itertools import combinations
+from math import comb
+from typing import List
+
+
+# My Solution
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        product_counts = Counter(a * b for a, b in combinations(nums, 2)).values()
+        return 8 * sum(comb(C, 2) for C in product_counts)
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        ans = 0
+        prods = {}
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                prod = nums[i] * nums[j]
+                if prod in prods:
+                    ans += prods[prod]
+                    prods[prod] += 1
+                else:
+                    prods[prod] = 1
+        return ans * 8
