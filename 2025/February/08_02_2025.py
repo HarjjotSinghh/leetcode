4a0cefc35c0b22200234090b29bac780d799a3ab
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-08 06:06:45 +0530

    8th February 2025

--- 2025/February/08_02_2025.py
+++ 2025/February/08_02_2025.py
@@ -1,1 +1,40 @@
-# This day has not yet come. This file is currently empty.
+from collections import defaultdict
+import heapq
+
+
+# My Solution
+class NumberContainers:
+    def __init__(self):
+        self.number_to_indices = defaultdict(list)
+        self.index_to_numbers = {}
+
+    def change(self, index: int, number: int) -> None:
+        self.index_to_numbers[index] = number
+        heapq.heappush(self.number_to_indices[number], index)
+
+    def find(self, number: int) -> int:
+        if not self.number_to_indices[number]:
+            return -1
+        while self.number_to_indices[number]:
+            index = self.number_to_indices[number][0]
+            if self.index_to_numbers.get(index) == number:
+                return index
+            heapq.heappop(self.number_to_indices[number])
+        return -1
+
+
+# Best / Most Optimal Solution
+class NumberContainers2:
+    def __init__(self):
+        self.index_val = {}
+        self.val_index = defaultdict(list)
+
+    def change(self, index: int, number: int) -> None:
+        self.index_val[index] = number
+        heapq.heappush(self.val_index[number], index)
+
+    def find(self, number: int) -> int:
+        h = self.val_index[number]
+        while h and self.index_val[h[0]] != number:
+            heapq.heappop(h)
+        return h[0] if h else -1
