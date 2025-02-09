d332b8ca17076909eb59d0fa8b72f352ffda4bb9
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-09 06:05:27 +0530

    9th February 2025

--- 2025/February/09_02_2025.py
+++ 2025/February/09_02_2025.py
@@ -1,1 +1,30 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+from itertools import starmap
+from collections import Counter
+from operator import sub
+
+
+# My Solution
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        bad_pairs = 0
+        diff_count = {}
+        for pos in range(len(nums)):
+            diff = pos - nums[pos]
+            good_pairs_count = diff_count.get(diff, 0)
+            bad_pairs += pos - good_pairs_count
+            diff_count[diff] = good_pairs_count + 1
+        return bad_pairs
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def countBadPairs(self, l: List[int]) -> int:
+        return (((len(l) << 1) - 1) ** 2 - 1 >> 3) - (
+            sum(
+                v * (v - 1)
+                for v in Counter(starmap(sub, enumerate(l))).values()
+                if v > 1
+            )
+            >> 1
+        )
