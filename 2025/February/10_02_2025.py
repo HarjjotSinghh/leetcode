59c11b2c4b00e1c979b7299f1194e093232973a3
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-10 05:35:25 +0530

    10th February 2025

--- 2025/February/10_02_2025.py
+++ 2025/February/10_02_2025.py
@@ -1,1 +1,27 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        answer = []
+        for char in s:
+            if char.isdigit() and answer:
+                answer.pop()
+            else:
+                answer.append(char)
+        return "".join(answer)
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def clearDigits(self, s: str) -> str:
+        letters = []
+        letters_to_delete = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i].isnumeric():
+                letters_to_delete += 1
+            else:
+                if letters_to_delete > 0:
+                    letters_to_delete -= 1
+                else:
+                    letters.append(s[i])
+        letters.reverse()
+        return "".join(letters)
