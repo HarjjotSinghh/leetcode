dd6e03e11c63cf25569457863b131a762da981ef
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-11 09:43:40 +0530

    11th February 2025

--- 2025/February/11_02_2025.py
+++ 2025/February/11_02_2025.py
@@ -1,1 +1,24 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack = []
+        part_length = len(part)
+        for char in s:
+            stack.append(char)
+            if len(stack) >= part_length and self._check_match(
+                stack, part, part_length
+            ):
+                for _ in range(part_length):
+                    stack.pop()
+        return "".join(stack)
+
+    def _check_match(self, stack: list, part: str, part_length: int) -> bool:
+        return "".join(stack[-part_length:]) == part
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        while part in s:
+            s = s.replace(part, "", 1)
+        return s
