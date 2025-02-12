c5599b0f71cbf17d2baf6ec1f23979e0ae0b370c
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-12 16:01:10 +0530

    12th February 2025

--- 2025/February/12_02_2025.py
+++ 2025/February/12_02_2025.py
@@ -1,1 +1,43 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        result = -1
+        digit_mapping = [0] * 82
+        for element in nums:
+            digit_sum = 0
+            temp_element = element
+            while temp_element:
+                temp_element, curr_digit = divmod(temp_element, 10)
+                digit_sum += curr_digit
+            if digit_mapping[digit_sum] > 0:
+                result = max(result, digit_mapping[digit_sum] + element)
+            digit_mapping[digit_sum] = max(digit_mapping[digit_sum], element)
+        return result
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def maximumSum(self, nums: List[int]) -> int:
+        d, mx = dict(), -1
+        for num in nums:
+            sm, n = 0, num
+            while n:
+                sm += n % 10
+                n //= 10
+            if sm in d:
+                if d[sm][0] <= num:
+                    d[sm][1] = d[sm][0]
+                    d[sm][0] = num
+                elif d[sm][1] < num:
+                    d[sm][1] = num
+            else:
+                d[sm] = [num, -1]
+        for k in d:
+            if d[k][1] != -1:
+                sm = d[k][0] + d[k][1]
+                if sm > mx:
+                    mx = sm
+        return mx
