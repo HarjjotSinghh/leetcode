849f57c1e0b91f9153ed55ddb3c8053d562e742d
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-18 05:31:58 +0530

    18th February 2025

--- 2025/February/18_02_2025.py
+++ 2025/February/18_02_2025.py
@@ -1,1 +1,28 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        result = []
+        previous_index = 0
+        for current_index in range(len(pattern) + 1):
+            result.append(str(1 + current_index))
+            if current_index == len(pattern) or pattern[current_index] == "I":
+                result[previous_index:] = reversed(result[previous_index:])
+                previous_index = current_index + 1
+        return "".join(result)
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def smallestNumber(self, pattern: str) -> str:
+        n = len(pattern)
+        res = [i for i in range(1, n + 2)]
+        i = 0
+        while i < n:
+            t = i
+            while t < n and pattern[t] == "D":
+                t += 1
+            res[i : t + 1] = reversed(res[i : t + 1])
+            if t != i:
+                i = t - 1
+            i += 1
+        return "".join(map(str, res))
