e5a976bf236675fcaa459efb283eee2884c536eb
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-20 05:32:12 +0530

    20th February 2025

--- 2025/February/20_02_2025.py
+++ 2025/February/20_02_2025.py
@@ -1,1 +1,20 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        ans = []
+        for i in range(len(nums)):
+            curr = nums[i][i]
+            ans.append("1" if curr == "0" else "0")
+        return "".join(ans)
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        ans = ""
+        for i, num in enumerate(nums):
+            ans += str(abs(int(num[i]) - 1))
+        return ans
