fff8e14d6c18f0607b8877963af708ef73431165
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-02-25 06:17:06 +0530

    25th February 2025

--- 2025/February/25_02_2025.py
+++ 2025/February/25_02_2025.py
@@ -1,1 +1,33 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+
+# My Solution
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        MOD = 10**9 + 7
+        count = prefix_sum = 0
+        odd_count = 0
+        even_count = 1
+        for num in arr:
+            prefix_sum += num
+            if prefix_sum % 2 == 0:
+                count += odd_count
+                even_count += 1
+            else:
+                count += even_count
+                odd_count += 1
+            count %= MOD
+        return count
+
+
+# Best / Most Optimal Solution
+class Solution2:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        cumSum = odd = even = 0
+        for num in arr:
+            cumSum += num
+            if cumSum % 2:
+                odd += 1
+            else:
+                even += 1
+        return odd * (even + 1) % (pow(10, 9) + 7)
