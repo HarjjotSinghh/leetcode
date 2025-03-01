ef64bf527f514110810e5c35ac24e145fe5ee9da
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-03-01 05:37:48 +0530

    1st March 2025

--- 2025/March/01_03_2025.py
+++ 2025/March/01_03_2025.py
@@ -1,1 +1,32 @@
-# This day has not yet come. This file is currently empty.
+# My Solution
+from typing import List
+
+
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        zeros=0
+        i=0
+        while(i<(len(nums)-1)):
+            if(nums[i]==nums[i+1]):
+                nums[i]*=2
+                nums[i+1]=0
+            i+=1
+        zeros=nums.count(0)
+        nums = [i for i in nums if i != 0]
+        return nums+([0]*zeros)
+
+# Best / Most Optimal Solution
+class Solution2:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        i=0
+        while i<len(nums)-1:
+            if nums[i]==nums[i+1]:
+                nums[i]*=2
+                nums[i+1]=0
+            i+=1
+        j=0
+        for k in range(len(nums)):
+            if nums[k]!=0:
+                nums[k],nums[j]=nums[j],nums[k]
+                j+=1
+        return nums
