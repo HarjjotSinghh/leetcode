ceea31917a4bb3a1ff88935d51471dce052af091
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-03-04 09:01:35 +0530

    4th March 2025

--- 2025/March/04_03_2025.py
+++ 2025/March/04_03_2025.py
@@ -1,1 +1,24 @@
-# This day has not yet come. This file is currently empty.
+from cmath import log
+
+# My Solution
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n > 0:
+            if n % 3 == 2:
+                return False
+            n //= 3
+        return True
+
+# Best / Most Optimal Solution
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        lastPowUsed = None
+        while n != 0:
+            logResult = log(n, 3)
+            decimalPart = logResult % 1
+            integerPart = int(logResult - decimalPart)
+            integerPart += 1 if decimalPart > 0.999 else 0
+            if integerPart == lastPowUsed: return False
+            lastPowUsed = integerPart
+            n -= 3**integerPart
+        return True
