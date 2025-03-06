8eb3e29afad11ae289ad76918b71bc5f2cd6973f
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-03-06 22:59:50 +0530

    6th March 2025

--- 2025/March/06_03_2025.py
+++ 2025/March/06_03_2025.py
@@ -16,3 +16,16 @@ class Solution:
         return [repeat, missing]
 
 # Best / Most Optimal Solution
+class Solution2:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        currSum = (n*n)*((n*n) +1)//2
+        seen = set()
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] not in seen:
+                    seen.add(grid[i][j])
+                    currSum -= grid[i][j]
+                else:
+                    dup = grid[i][j]
+        return [dup, currSum]
