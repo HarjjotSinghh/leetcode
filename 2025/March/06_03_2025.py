55fc2c29d10f322f553fc58fe65a39cf46a223d4
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-03-06 06:06:16 +0530

    6th March 2025

--- 2025/March/06_03_2025.py
+++ 2025/March/06_03_2025.py
@@ -1,1 +1,18 @@
-# This day has not yet come. This file is currently empty.
+from typing import List
+
+# My Solution
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        freq = {}
+        for row in grid:
+            for num in row:
+                freq[num] = freq.get(num, 0) + 1
+        for num in range(1, n * n + 1):
+            if num not in freq:
+                missing = num
+            elif freq[num] == 2:
+                repeat = num
+        return [repeat, missing]
+
+# Best / Most Optimal Solution
