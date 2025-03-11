503186a74e31e598059aeb70b0486285211a51dc
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-03-11 09:02:24 +0530

    11th March 2025

--- 2025/March/11_03_2025.py
+++ 2025/March/11_03_2025.py
@@ -1,1 +1,24 @@
-# This day has not yet come. This file is currently empty.
+
+# My Solution
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        last_pos = [-1] * 3
+        total = 0
+        for pos in range(len(s)):
+            last_pos[ord(s[pos]) - ord("a")] = pos
+            total += 1 + min(last_pos)
+        return total
+
+# Best / Most Optimal Solution
+__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        res = i = 0
+        d = {c:0 for c in 'abc'}
+        for j in range(len(s)):
+            d[s[j]] += 1
+            while all(d.values()):
+                d[s[i]] -= 1
+                i += 1 
+            res += i
+        return res
